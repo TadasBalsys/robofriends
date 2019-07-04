17ab81ab23ca83f18a0d47945c4aa6e1b063913d
Tadas Balsys <balsys.tadas@gmail.com>
2019-07-04 21:04:29 +0300

    Installed bootstrap, jquery, popper.js. Created Card component and jsonPlaceholder local data at robot.js

---
+++ src/Card.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+
+const cardStyle = {
+  width: "16rem"
+};
+
+const Card = ({robot:{name, email, id}}) => {
+  return (
+    <div className="card m-2 bg-warning" style={cardStyle}>
+      <img
+        className="card-img-top"
+        src={`https://robohash.org/${id}?size=150x150`}
+        alt=""
+      />
+      <div className="card-body">
+        <h1 className="card-title">{name}</h1>
+        <p className="card-text">{email}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import Card from "./Card";
+import "bootstrap/dist/css/bootstrap.min.css";
+import $ from "jquery";
+import Popper from "popper.js";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<Hello greeting={"Hello React Ninja"}/>, document.getElementById('root'));
+import { robots } from "./robots";
+
+// const  getRobots = fetch("https://jsonplaceholder.typicode.com/users")
+//   .then(res => res.json())
+//   .then(data => console.log(data));
+
+ReactDOM.render(
+  <div className="container">
+    <div className="row">
+      <Card robot={robots[0]} />
+      <Card robot={robots[1]} />
+      <Card robot={robots[2]} />
+    </div>
+  </div>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
