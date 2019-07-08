eb745b102e8d1d7ee879b75167c02194072ada79
Tadas Balsys <balsys.tadas@gmail.com>
2019-07-08 20:22:01 +0300

    Created store in index.js from reducer(searchRobots). Wrapped App container with Provider component from react-redux.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
+// Redux 
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+
 import App from "../src/containers/App";
+import { searchRobots } from "./reducers";
 import "./index.css"
 import "./card.css";
 
@@ -12,13 +17,17 @@ import "bootstrap/dist/js/bootstrap.bundle.min";
 
 import * as serviceWorker from "./serviceWorker";
 
-// import { robots } from "./robots";
+// const  store is Redux Store - place were all states is.
+const store = createStore(searchRobots);
 
-// const  getRobots = fetch("https://jsonplaceholder.typicode.com/users")
-//   .then(res => res.json())
-//   .then(data => console.log(data));
-
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+    // Provider component from react-redux will pass store (a.k.a State) to all components down to component tree
+    // The store us is rootReducer or what ever reducers (this case searchRobots) to create the store.
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
