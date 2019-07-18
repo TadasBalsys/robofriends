fa8bddb2cf51f8387b5e7893d4f9b7a758fdcc6a
Tadas Balsys <balsys.tadas@gmail.com>
2019-07-18 15:25:55 +0300

    Added favicons for PWA, code a manifest.json. Completed PWA.

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -5,7 +5,7 @@ const SearchBox = ({searchField, searchChange}) => {
   
   return (
     <div className="col-12 text-center my-4">
-      <input className=' mt-3' type="search" placeholder="Search Robots" onChange={searchChange} />
+      <input className=' mt-3' aria-label="Search for Robots" type="search" placeholder="Search Robots" onChange={searchChange} />
     </div>
   );
 };
