2e852096fa09664e8892d79bd32faeb317d49177
Tadas Balsys <balsys.tadas@gmail.com>
2019-07-18 11:15:47 +0300

    Updates

---
+++ precache-manifest.a73cf5cabcc7d29345add8691ede3589.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16ce39ac9005ebbfbaaf6e89d753bc99",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "9f11c359163953df3f7f",
+    "url": "/robofriends/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "7ea01052a429eb70e17e",
+    "url": "/robofriends/static/css/main.f51b9326.chunk.css"
+  },
+  {
+    "revision": "9f11c359163953df3f7f",
+    "url": "/robofriends/static/js/2.085d6f29.chunk.js"
+  },
+  {
+    "revision": "7ea01052a429eb70e17e",
+    "url": "/robofriends/static/js/main.165d836e.chunk.js"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
